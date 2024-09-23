eeb82138f8476a4f598b1c8488a21d2b8be65dc8
swelanaugsute <swelanauguste@gmail.com>
2024-09-23 13:59:12 -0400

    entrypoint

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -11,13 +11,12 @@ then
     echo "PostgreSQL started"
 fi
 
-python manage.py migrate
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --noinput
+# python manage.py createsuperuser --noinput
 python manage.py collectstatic --noinput
-python manage.py add_users
-python manage.py add_fake_data
-python manage.py add_fake_more_data
+# python manage.py add_users
+# python manage.py add_fake_data
+# python manage.py add_fake_more_data
 
 exec "$@"
